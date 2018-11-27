f404c57e463a4ad426b0c4461b44dda3ece62b8a
Mark McDowall <mark.mcdowall@gmail.com>
2018-11-27 16:58:08 +0000

    Added in more tests and have changed the check to ensure that $rxrefs is defined as well as having elements within the array

--- modules/Bio/EnsEMBL/Xref/DBSQL/BaseAdaptor.pm
+++ modules/Bio/EnsEMBL/Xref/DBSQL/BaseAdaptor.pm
@@ -532,7 +532,7 @@ sub get_valid_codes {
 sub upload_xref_object_graphs {
   my ( $self, $rxrefs ) = @_;
 
-  if ( !( scalar @{$rxrefs} ) ) {
+  if ( !defined $rxrefs || !( scalar @{$rxrefs} ) ) {
     confess "Please give me some xrefs to load";
   }
 
@@ -541,19 +541,19 @@ sub upload_xref_object_graphs {
       confess "Your xref does not have an accession-number\n";
     }
     if ( !( defined $xref->{SOURCE_ID} ) ) {
-      confess "your xref: $xref->{ACCESSION} does not have a source-id\n";
+      confess "Your xref: $xref->{ACCESSION} does not have a source-id\n";
     }
 
     # Create entry in xref table and note ID
-    my $xref_id = $self->add_xref( (
+    my $xref_id = $self->add_xref( {
       "acc"        => $xref->{ACCESSION},
       "version"    => $xref->{VERSION} // 0,
       "label"      => $xref->{LABEL}   // $xref->{ACCESSION},
       "desc"       => $xref->{DESCRIPTION},
       "source_id"  => $xref->{SOURCE_ID},
       "species_id" => $xref->{SPECIES_ID},
-      "info_type"  => $xref->{INFO_TYPE} ),
-      "update_label" => 1, "update_desc" => 1 );
+      "info_type"  => $xref->{INFO_TYPE},
+      "update_label" => 1, "update_desc" => 1 } );
 
     # If there are any direct_xrefs, add these to the relevant tables
     $self->add_multiple_direct_xrefs( $xref );
